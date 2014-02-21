91f4a9302efd2ba207f2ffaa3c8f45a00d37999f
arnaudbey <arnaud.bey@gmail.com>
2014-02-21 16:05:34 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# What is it ?
+
+a small bash script who dl the lastest nightly of firefox for linux and install it in opt/ dir, and create the symlink  usr/local/bin/firefox-nightly
+
+# How-to
+
+Clone the projet
+> git clone https://github.com/arnaudbey/ff-nightly-dl-and-install.git
+> cd ff-nightly-dl-and-install.git
+
+if you want to run this script later from anywhere
+> sudo ln -s ./ff-nightly_dl_and_install.sh /usr/bin/firefox-nightly-update
+
+launch the script
+> sh ./ff-nightly_dl_and_install.sh
+
+or 
+
+> firefox-nightly-update
+
+
+
+You can now launch ff last nighly build through *firefox-nightly* command
---
+++ ff-nightly_dl_and_install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## Download the version depending the OS. 32 / 64 b
+if [ "uname -m | grep i686" ] 
+then 
+wget -P /tmp/ff-nightly/ $(wget -O - http://nightly.mozilla.org | egrep -o 'href=.*?linux-i686\.tar\.bz2.*?['"'"'"]' | sed -e 's/^href=//' -e 's/["'"'"']//g')
+else
+wget -P /tmp/ff-nightly/ $(wget -O - http://nightly.mozilla.org | egrep -o 'href=.*?linux-x86_64\.tar\.bz2.*?['"'"'"]' | sed -e 's/^href=//' -e 's/["'"'"']//g')
+fi
+
+## create opt/firefox-nightly
+if [ ! -d "/opt/firefox-nightly" ] 
+then
+	sudo mkdir /opt/firefox-nightly
+fi
+
+## untar the downloaded archive
+sudo tar -C /opt/firefox-nightly/ -xvjf /tmp/ff-nightly/*.tar.bz2
+
+## create symlink
+if [ ! -f "/usr/local/bin/firefox-nightly" ] 
+then
+	sudo ln -s /opt/firefox-nightly/firefox/firefox /usr/local/bin/firefox-nightly
+fi
+
+## clear tmp files
+rm -rf /tmp/ff-nightly/*
