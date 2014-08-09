76b8cf6b0491e2b160d76988e79fab3b218445d4
arnaudbey <arnaud.bey@gmail.com>
2014-08-09 21:09:38 +0200

    release is now choosable

--- ff-nightly_dl_and_install.sh
+++ ff-nightly_dl_and_install.sh
@@ -1,27 +1,51 @@
 #!/bin/bash
 
-## Download the version depending the OS. 32 / 64 b
-if  uname -m| grep -q "i686" ;
-then 
-	wget -P /tmp/ff-nightly/ $(wget -q -O - http://nightly.mozilla.org | egrep -o 'href=.*?firefox.*?linux-i686\.tar\.bz2.*?['"'"'"]' | sed -e 's/^href=//' -e 's/["'"'"']//g')
+echo "Choose the Firefox release you wanna install/upgrade [1|2|3|4]:"
+echo "1. Firefox Stable"
+echo "2. Firefox Beta"
+echo "3. Firefox Aurora"
+echo "4. Firefox Nightly"
+read -rp "> " CHOICE
+
+if  uname -m| grep -q "i686" ; then 
+	ARCHI="i686"
+	ARCHI_SHORT=""
 else
-	wget -P /tmp/ff-nightly/ $(wget -q -O - http://nightly.mozilla.org | egrep -o 'href=.*?firefox.*?linux-x86_64\.tar\.bz2.*?['"'"'"]' | sed -e 's/^href=//' -e 's/["'"'"']//g')
+	ARCHI="x86_64"
+	ARCHI_SHORT= "64"
 fi
 
-## create opt/firefox-nightly
-if [ ! -d "/opt/firefox-nightly" ] 
+if [ $CHOICE = 1 ]; then
+	RELEASE=""
+	URL=$(wget -q -O - https://www.mozilla.org/en-US/firefox/all/ | egrep -o "href=.*?os=linux${ARCHI_SHORT}&amp;lang=en-US['"'"'"]" | sed -e 's/\&amp;/\&/g' -e 's/^href=//' -e 's/["'"'"']//g' )
+elif [ $CHOICE = 2 ]; then
+	RELEASE="-beta"
+	URL=$(wget -q -O - https://www.mozilla.org/en-US/firefox/beta/all/ | egrep -o "href=.*?os=linux${ARCHI_SHORT}&amp;lang=en-US['"'"'"]" | sed -e 's/\&amp;/\&/g' -e 's/^href=//' -e 's/["'"'"']//g' )
+elif [ $CHOICE = 3 ]; then
+	RELEASE="-aurora"
+	URL=$(wget -q -O - https://www.mozilla.org/en-US/firefox/aurora/all/ | egrep -o "href=.*?firefox.*?en-US.linux-${ARCHI}\.tar\.bz2.*?['"'"'"]" | sed -e 's/^href=//' -e 's/["'"'"']//g')
+elif [ $CHOICE = 4 ]; then
+	URL=$(wget -q -O - http://nightly.mozilla.org | egrep -o "href=.*?firefox.*?linux-${ARCHI}\.tar\.bz2.*?['"'"'"]" | sed -e 's/^href=//' -e 's/["'"'"']//g')
+	RELEASE="-nightly"
+else
+	exit
+fi
+
+if [ ! -d "/tmp/ff$RELEASE" ] ; then
+	mkdir /tmp/ff$RELEASE
+else
+	rm -rf /tmp/ff$RELEASE/*
+fi
+
+if [ ! -d "/opt/firefox$RELEASE" ] 
 then
-	sudo mkdir /opt/firefox-nightly
+	sudo mkdir /opt/firefox$RELEASE
 fi
 
-## untar the downloaded archive
-sudo tar -C /opt/firefox-nightly/ -xvjf /tmp/ff-nightly/*.tar.bz2
+wget $URL -O /tmp/ff$RELEASE/ff$RELEASE.tar.bz2
+sudo tar -C /opt/firefox$RELEASE/ -xvjf /tmp/ff$RELEASE/*.tar.bz2
 
-## create symlink
-if [ ! -f "/usr/local/bin/firefox-nightly" ] 
+if [ ! -f "/usr/local/bin/firefox$RELEASE" ] 
 then
-	sudo ln -s /opt/firefox-nightly/firefox/firefox /usr/local/bin/firefox-nightly
+	sudo ln -s /opt/firefox$RELEASE/firefox/firefox /usr/local/bin/firefox$RELEASE
 fi
-
-## clear tmp files
-rm -rf /tmp/ff-nightly/*
